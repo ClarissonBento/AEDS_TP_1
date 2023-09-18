1f4938b2a5fb389ffbcbad5885692b582b7aecf4
Clarisson <clarisson.bento@ufv.br>
2023-09-17 21:30:31 -0300

    Primeiras funções do tad carta adicionadas.

--- carta.c
+++ carta.c
@@ -1,1 +1,21 @@
 #include "carta.h"
+
+Criar_carta(Carta *carta, Naipe naipe, unsigned valor, Posicao posi){
+    carta->naipe = naipe;
+    carta->valor = valor;
+    carta->posi = posi;
+}
+
+
+// FUNÇÕES QUE RETORNAM VALORES (GET)
+Naipe__retorna(Carta *carta){
+    return carta->naipe;
+}
+
+Valor__retorna(Carta *carta){
+    return carta->valor;
+}
+
+Posicao__retorna(Carta *carta){
+    return carta->posi;
+}
--- carta.h
+++ carta.h
@@ -2,12 +2,24 @@
 #define CARTA_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef enum {COPAS, ESPADADAS, OUROS, PAUS} Naipe;
 
-typedef struct {
-    const Naipe naipe;
+typedef enum {CIMA, BAIXO} Posicao;
 
+unsigned valor = {'A',2,3,4,5,6,7,8,9,10,'J','Q','K'};
+
+// Naipe e valor são const
+typedef struct {
+    Naipe naipe;
+    unsigned valor;
+    Posicao posi;
 }Carta;
+
+Criar_carta(Carta *carta, Naipe naipe, unsigned valor, Posicao posi);
+Naipe__retorna(Carta *carta);
+Valor__retorna(Carta *carta);
+Posicao__retorna(Carta *carta);
 
 #endif
