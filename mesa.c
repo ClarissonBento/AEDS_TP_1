c2f2c41dfc7bc6c0038ed0ba719783c6126b7949
KozukiIaguin <iago.falcao@ufv.com>
2023-09-30 16:57:35 -0300

    compra

--- mesa.c
+++ mesa.c
@@ -71,29 +71,33 @@ void preparar(Mesa *mesa) {
     // Inicialize o tableau
     for (int i = 0; i < 7; i++) {
         Lista_cartas *coluna_tableau = &(mesa->tableau[i]);
-        while (i >= num_cartas_coluna) {
+        while (i > num_cartas_coluna) {
             Carta c;
-            c = Topo__retorna(&baralho);
+            c = Topo__retorna(baralho);
+            Topo__remove(baralho,&c);
             Topo__adiciona(coluna_tableau, &c);
             num_cartas_coluna++;
         }
     }
 }
 
-/*
+
+
 //compra de cartas
-Carta compra_carta(Lista_cartas *lista){
-
-    if(taVazia(lista)){
-        printf("Nao e possivel comprar cartas pois o baralho acabou");
-        return;
+Carta compra_carta(Lista_cartas *lista) {
+    if (taVazia(lista)) {
+        printf("Não é possível comprar cartas, pois o baralho acabou.\n");
+        //caso o baralho esteja vazio, retornar uma carta nula
+        Carta carta_vazia;
+        carta_vazia.naipe = nao_definido;
+        carta_vazia.valor = nao_definido;
+        carta_vazia.posicao = nao_definido;
+        return carta_vazia;
+    } else {
+        Carta c = lista->p_Primeiro->carta;
+        lista->p_Primeiro = lista->p_Primeiro->proximo;
+        return c;
     }
-    else {
-   Carta c = lista->p_Primeiro->carta;
-   lista->p_Primeiro=lista->p_Primeiro->proximo;
-   return c;
-    }
-
 }
 
 /*
